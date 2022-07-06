d30761a1d71251d8d7635add75a3c56862b06e5c
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-07-06 12:54:37 -0300

    header mobile navbar offcanvas

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,35 @@ const Header = () => {
 
     return(
         <header>
-            Header
+        
+            <nav class="navbar">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">MUSIC</a>
+                    <button class="navbar-toggler d-block d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="offcanvas offcanvas-end d-block d-md-none" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">MUSIC</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li class="nav-item">
+                                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link 1</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link 2</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
         </header>
     );
 
