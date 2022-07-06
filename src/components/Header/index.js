ee297e46c9c65704428747fdbc82f668a279794e
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-07-06 14:47:25 -0300

    ajustes layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,27 +7,27 @@ const Header = () => {
     return(
         <header>
         
-            <nav class="navbar">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">MUSIC</a>
-                    <button class="navbar-toggler d-block d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">MUSIC</a>
+                    <button className="navbar-toggler d-block d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="offcanvas offcanvas-end d-block d-md-none" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                        <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">MUSIC</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <div className="offcanvas offcanvas-end d-block d-md-none" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">MUSIC</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
-                        <div class="offcanvas-body">
-                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                <li class="nav-item">
-                                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li className="nav-item">
+                                    <a className="nav-link" aria-current="page" href="#">Home</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Link 1</a>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Link 1</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Link 2</a>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Link 2</a>
                                 </li>
                             </ul>
                         </div>
