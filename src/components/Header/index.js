aca5c35550ab9b5b85f0f4347e236d11c80fd604
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-07-08 20:05:56 -0300

    navbar mobile
    estruturação e parte funcional da navbar mobile e alteração no header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,33 +10,15 @@ const Header = () => {
     return(
         <header>
         
-            <nav className="navbar">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">MUSIC</Link>
-                    <button className="navbar-toggler d-block d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="offcanvas offcanvas-end d-block d-md-none" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                        <div className="offcanvas-header">
-                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">MUSIC</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                        </div>
-                        <div className="offcanvas-body">
-                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                <li className="nav-item" data-bs-dismiss="offcanvas">
-                                    <Link className="nav-link" to="/">Início</Link>
-                                </li>
-                                <li className="nav-item" data-bs-dismiss="offcanvas">
-                                    <Link className="nav-link" to="/playlist">Tocando agora</Link>
-                                </li>
-                                <li className="nav-item" data-bs-dismiss="offcanvas">
-                                    <Link className="nav-link" to="/liked">Músicas curtidas</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </nav>
+        <nav className="navbar">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                {/* <img src="" alt="" width="30" height="24" className="d-inline-block align-text-top" /> */}
+                    <i className="bi bi-music-note-beamed me-2"></i>
+                    MUSIC
+                </Link>
+            </div>
+        </nav>
 
         </header>
     );
--- src/components/MobileNav/index.js
+++ src/components/MobileNav/index.js
@@ -1,11 +1,66 @@
 //Css
 import './mobileNav.css';
 
+//Imports
+import { Link } from 'react-router-dom';
+
+
 const MobileNav = () => {
 
     return(
         <div className='mobileNav'>
-            mobile nav
+
+            {/* ------------------- Botões ------------------- */}
+            <button>
+                <Link to="/">
+                    <i className="bi bi-house-fill"></i>
+                </Link>
+            </button>
+            
+            <button>
+                <Link to="/liked">
+                    <i className="bi bi-heart-fill"></i>
+                </Link>
+            </button>
+
+            <button>
+                <Link to="/playlist">
+                    <i className="bi bi-music-note-list"></i>
+                </Link>
+            </button>
+
+
+            {/* ------------------- Navbar OffCanvas ------------------- */}
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <button className="navbar-toggler d-block d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                        <i className="bi bi-three-dots"></i>
+                    </button>
+                    <div className="offcanvas offcanvas-end d-block d-md-none" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                                <i className="bi bi-music-note-beamed me-2"></i>
+                                MUSIC
+                            </h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3 text-end">
+                                <li className="nav-item" data-bs-dismiss="offcanvas">
+                                    <Link className="nav-link" to="/">Link 1</Link>
+                                </li>
+                                <li className="nav-item" data-bs-dismiss="offcanvas">
+                                    <Link className="nav-link" to="/playlist">Link 2</Link>
+                                </li>
+                                <li className="nav-item" data-bs-dismiss="offcanvas">
+                                    <Link className="nav-link" to="/liked">Link 3</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
         </div>
     );
 
