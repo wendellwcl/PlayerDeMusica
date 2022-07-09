c17d6c1a2864122f53196cca20bb9a0be61d8ad0
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-07-09 14:15:08 -0300

    estilização do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,17 +9,15 @@ const Header = () => {
 
     return(
         <header>
-        
-        <nav className="navbar">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">
-                {/* <img src="" alt="" width="30" height="24" className="d-inline-block align-text-top" /> */}
-                    <i className="bi bi-music-note-beamed me-2"></i>
-                    MUSIC
-                </Link>
-            </div>
-        </nav>
-
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">
+                    {/* <img src="" alt="" width="30" height="24" className="d-inline-block align-text-top" /> */}
+                        <i className="bi bi-music-note-beamed me-2"></i>
+                        Music
+                    </Link>
+                </div>
+            </nav>
         </header>
     );
 
