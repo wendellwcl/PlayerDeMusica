39a9e8c3e9f097b32b6b1353cda14213449f7692
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-07-10 15:28:12 -0300

    switch dark mode

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,12 +10,17 @@ const Header = () => {
     return(
         <header>
             <nav className="navbar">
-                <div className="container-fluid">
+                <div className="container">
                     <Link className="navbar-brand" to="/">
                     {/* <img src="" alt="" width="30" height="24" className="d-inline-block align-text-top" /> */}
-                        <i className="bi bi-music-note-beamed me-2"></i>
+                        <i className="bi bi-music-note-beamed"></i>
                         Music
                     </Link>
+                    <label id="switch-container" htmlFor="switch-checkbox-darkmode">
+                        <span id='switch-text'>Dark mode</span>
+                        <input type="checkbox" id="switch-checkbox-darkmode" />
+                        <span id='switch-darkmode'></span>
+                    </label>
                 </div>
             </nav>
         </header>
