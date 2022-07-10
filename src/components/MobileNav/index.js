4110f369617e53c13a326ee43c7cf0462fb25f9a
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-07-10 14:08:51 -0300

    ajustes navbar mobile >> >> >> ajustes de acessibilidade na navbar mobile

--- src/components/MobileNav/index.js
+++ src/components/MobileNav/index.js
@@ -14,18 +14,21 @@ const MobileNav = () => {
             <button>
                 <Link to="/">
                     <i className="bi bi-house-fill"></i>
+                    Início
                 </Link>
             </button>
             
             <button>
                 <Link to="/liked">
                     <i className="bi bi-heart-fill"></i>
+                    Curtidas
                 </Link>
             </button>
 
             <button>
                 <Link to="/playlist">
                     <i className="bi bi-music-note-list"></i>
+                    Tocando
                 </Link>
             </button>
 
@@ -40,14 +43,14 @@ const MobileNav = () => {
                         <div className="offcanvas-header">
                             <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
                                 {/* <img src="" alt="" width="30" height="24" className="d-inline-block align-text-top" /> */}
-                                <i className="bi bi-music-note-beamed me-2"></i>
+                                <i className="bi bi-music-note-beamed"></i>
                                 Music
                             </h5>
                             <button type="button" className='btn-close' data-bs-dismiss="offcanvas" aria-label="Close">
                                 <i class="bi bi-x-lg"></i>
                             </button>
                         </div>
-                        <div className="offcanvas-body">
+                        <div className="offcanvas-body g-0">
                             <ul className="navbar-nav justify-content-end flex-grow-1 pe-3 text-end">
                                 <li className="nav-item" data-bs-dismiss="offcanvas">
                                     <Link className="nav-link" to="/">Link 1</Link>
