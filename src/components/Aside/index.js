358258749b6d17f9310b9ed4bba0cb2375dbf1e6
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-07-10 13:33:04 -0300

    estilização aside

--- src/components/Aside/index.js
+++ src/components/Aside/index.js
@@ -12,13 +12,22 @@ const Aside = () => {
 
             <ul className='nav flex-column'>
                 <li className='nav-item'>
-                    <Link className='nav-link' to="/">Início</Link>
+                    <Link className='nav-link' to="/">
+                        <i className="bi bi-house-fill"></i>
+                        Início
+                    </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link className='nav-link' to="/playlist">Tocando agora</Link>
+                    <Link className='nav-link' to="/playlist">
+                        <i className="bi bi-music-note-list"></i>
+                        Tocando agora
+                    </Link>
                 </li>
                 <li className='nav-item'>
-                    <Link className='nav-link' to="/liked">Músicas curtidas</Link>
+                    <Link className='nav-link' to="/liked">
+                        <i className="bi bi-heart-fill"></i>
+                        Músicas curtidas
+                    </Link>
                 </li>
             </ul>
 
