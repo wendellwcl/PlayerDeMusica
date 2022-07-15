7b4589183bc45b5bf925c59bf7db35a8c44d1c05
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-07-15 17:47:17 -0300

    barra de progresso
    barra de progresso funcional: atualização em tempo real, funções para mudar o tempo da música

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -2,32 +2,96 @@
 import './player.css';
 
 //Assets
-import SmileForMe from '../../assets/musics/smile-for-me-kidcut-main-version-01-26-3177.mp3'
+import SmileForMe from '../../assets/musics/smile-for-me-kidcut-main-version-01-26-3177.mp3';
+import Oly from '../../assets/musics/oly-walz-main-version-01-06-12377.mp3';
+import JumpAround from '../../assets/musics/jump-around-all-good-folks-main-version-00-49-750.mp3';
 
 //Imports
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 
 const Player = () => {
 
-    const [ play, setPlay] = useState(false);
-    const liked = false
+    //Referenciando Audio
+    const audioEl = useRef();
+    const music = audioEl.current;
+    
+    //Referenciando Barra de Progresso
+    const progressBarEl = useRef();
+    const progressBar = progressBarEl.current;
 
+
+    //States
+    const [ duration, setDuration ] = useState();
+    const [ durationPercentage, setDurationPercentage ] = useState();
+    const [ currentTime, setCurrentTime ] = useState();
+    const [ inputValue, setInputValue ] = useState(0);
+    const [ play, setPlay ] = useState(false);
+    const liked = false;
+
+
+    //Play e Pause
     function handleMusic(){
-        const music = document.querySelector('#music');
         play ? music.pause() : music.play();
         setPlay(!play);
     }
 
+    //Atualizar Progresso
     function handleProgress(value){
-        const progressBar = document.querySelector('#progress-bar');
-        progressBar.style.width = `${value}%`;
+        //Atualizar barra e input
+        let progress = Math.floor(value / durationPercentage);
+        progressBar.style.width = `${progress}%`;
+        setInputValue(progress);
+        
+        //Atualizar tempo atual
+        let { minutes, seconds } = timeToMinutes(value);
+        setCurrentTime(`${minutes}:${seconds}`);
     };
+    
+    //Setar/atualizar tempo da música ao clicar no input de progresso
+    function setMusicTime(value){
+        music.currentTime = value * durationPercentage;
+        music.play();
+        if(play === false){
+            setPlay(true);
+        };
+    };
+
+    //Setar/atualizar duração da musica
+    function handleDuration(){
+        let time = music.duration
+        let { minutes, seconds } = timeToMinutes(time);
+        setDuration(`${minutes}:${(seconds)}`);
+        setDurationPercentage(time / 100);
+    };
+
+    //Ações quando a música terminar
+    function handleEnd(){
+        setPlay(false);
+        setInputValue(0);
+        setCurrentTime(null);
+        progressBar.style.width = '0%';
+    };
+
+    function timeToMinutes(time){
+        let minutes = Math.floor(time / 60);
+        let seconds = Math.floor(time % 60);
+        seconds = (seconds.toString()).padStart(2, '0');
+        return { minutes, seconds };
+    };
+
 
     return(
         <section id="player">
 
-            <audio src={SmileForMe} id='music'></audio>
+            <audio 
+                src={JumpAround} 
+                ref={audioEl} 
+                onTimeUpdate={e => handleProgress(e.target.currentTime)} 
+                onLoadedMetadata={() => handleDuration()}
+                onEnded={() => handleEnd()}
+                >
+            </audio>
             
             <div id='player-controls'>
 
@@ -51,10 +115,17 @@ const Player = () => {
                 </div>
 
                 <div id='progress-container'>
-                    <input type="range" min='0' max='100' step='1' id='input-progress' onChange={e => handleProgress(e.target.value)}/>
-                    <div id='progress-bar'></div>
-                    <span id='current-time'>--:--</span>
-                    <span id='duration'>--:--</span>
+                    <input 
+                        type="range" 
+                        min='0' 
+                        max='100' 
+                        value={inputValue} 
+                        id='input-progress'  
+                        onChange={e => setMusicTime(e.target.value)} 
+                    />
+                    <div id='progress-bar' ref={progressBarEl}></div>
+                    <span id='current-time'>{currentTime ? currentTime : '0:00'}</span>
+                    <span id='duration'>{duration ? duration : '0:00'}</span>
                 </div>
 
             </div>
